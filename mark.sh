e25838530ac1b1a9645584f2755373dd6acbc247
gabriel <blanckaertgabriel@Gabriel>
2026-05-04 14:46:39 +0200

    limite note 0-20

--- mark.sh
+++ mark.sh
@@ -13,7 +13,10 @@ factorielle0;
 checkIndentation;
 negativefactorielle;
 nombreParametre;
+
 note=$(echo "$points - $malus" | bc)
+note=$(echo "if ($note < 0) 0 else if ($note > 20) 20 else $note" | bc)
+
 generateCSV $note;
 
 echo "points : $points";
